d3c3ec20f4cfcd40ce679ab5badfe7e4e38068b2
juancsr <jcsarmientor@correo.udistrital.edu.co>
2021-12-03 17:30:48 -0500

    checking cliente and producto exists before creating new pedido

--- api_pedidos/app.py
+++ api_pedidos/app.py
@@ -40,6 +40,13 @@ def update_producto(producto: object) -> bool:
         return False
     return True
 
+def checkIfProductExists(id: int) -> bool:
+    r = req.get('{}/api/products'.format(api_productos_url))
+    for producto in r.json():
+        if producto['Id'] == id:
+            return True
+    return False
+
 @app.route('/cliente/<int:idCliente>', methods=['GET'])
 def getPedidoByIdCliente(idCliente):    
     pedido = Pedido.objects.get(cliente_id=idCliente).to_mongo().to_dict()
@@ -48,6 +55,8 @@ def getPedidoByIdCliente(idCliente):
     productos = []
     for producto in pedido['productos']:
         producto_info =info_producto(producto['id'])
+        print(producto, producto_info)
+        producto_info['quantity'] = producto['cantidad']
         producto = producto_info
         productos.append(producto)
     pedido['productos'] = productos
@@ -56,15 +65,27 @@ def getPedidoByIdCliente(idCliente):
 @app.route('/', methods=['POST'])
 def newPedido():
     body = request.get_json()
-    print(body)
-    for producto in body['productos']:
+    try:
+        info_cliente(body['cliente_id'])  
+    except:
+        return "El cliente no existe", 404
+    
+    i_productos = []
+    for producto in body['productos']:        
+        print(producto['id'])
+        if not checkIfProductExists(producto['id']):
+            return "El producto no existe", 404
+
         i_producto = info_producto(producto['id'])
-        print(i_producto)
+        
         if i_producto['quantity'] < producto['cantidad']:
             return "No hay suficientes existencias de {} para crear el pedido".format(i_producto['name']), 404
         i_producto['quantity'] = i_producto['quantity'] - producto['cantidad']
-        update_producto(i_producto)
+        i_productos.append(i_producto)
     Pedido(**body).save()
+
+    for i_producto in i_productos:
+        update_producto(i_producto)
     return "Ok", 200
 
 @app.route('/', methods=['GET'])
@@ -78,6 +99,7 @@ def allPedidos():
         productos = []
         for producto in pedido['productos']:
             producto_info =info_producto(producto['id'])
+            producto_info['quantity'] = producto['cantidad']
             producto = producto_info
             productos.append(producto)
         pedido['productos'] = productos
