951c972ce1f9f2d87d05c344e678ea3a66cc79cf
juancsr <jcsarmientor@correo.udistrital.edu.co>
2021-12-03 17:29:58 -0500

    removed unique for cliente_id in pedidos

--- api_pedidos/models.py
+++ api_pedidos/models.py
@@ -2,5 +2,5 @@ from db import db
 
 class Pedido(db.Document):
     codigo = db.StringField(required=True, unique=True)
-    cliente_id = db.IntField(required=True, unique=True)
+    cliente_id = db.IntField(required=True)
     productos = db.ListField(db.DictField(), required=True)
